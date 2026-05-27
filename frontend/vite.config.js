8b1224c2b75ced356be588c85fd20a76ed867daa
Bilal709 <mb9174547@gmail.com>
2026-05-27 15:47:18 -0700

    force rebuild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://task-manager-api-8ai2.onrender.com')
+  }
+})
